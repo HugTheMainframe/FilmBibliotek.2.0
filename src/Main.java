b875546b6c09dbc9a29557ea48dca58552759db1
HugTheMainframe <rasm65n4@stud.kea.dk>
2024-02-22 01:02:32 +0100

    Update: User Storie 2: as a user i would like to have a menu so i can choose between 1 creating a movie or 2 quitting. log: Created a while loop to make a menu reappear until quitting. the loop creates a new instaces of the movie object and add it to the MovieCollection class array.

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,52 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //inout object plus user input fields per movie
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the title of the film: ");
-        String title = input.nextLine();
-        System.out.print("Enter the director of the film: ");
-        String director = input.nextLine();
-        System.out.print("Enter the year of the film: ");
-        int yearCreated = input.nextInt();
-        System.out.print("Enter if the film is in color(true or false): ");
-        boolean isInColor = input.nextBoolean();
-        System.out.print("Enter the length of the film(in minuts): ");
-        int lengthInMinuts = input.nextInt();
-        System.out.print("Enter the genre of the film: ");
-        //must be next else it skips the input
-        String genre = input.next();
+        MovieCollection movieList = new MovieCollection();
+        int i = 0;
 
-        //testing object for input
-        Movie movieOne = new Movie(title, director, yearCreated,
-                isInColor, lengthInMinuts, genre);
-//
-//
-        //testing for output of the objects getters
-//        System.out.println(movieOne.getTitle() + "\n" + movieOne.getDirector() + "\n" +
-//                movieOne.getYearCreated() + "\n" + movieOne.getIsinColor() + "\n" +
-//                 movieOne.getLengthInMinuts() + "\n" + movieOne.getGenre());
+        while(true) {
+            System.out.println("Welcome to my movie collection");
+            System.out.println("1. Add a Movie");
+            System.out.println("2. Quit");
+            int dicision = input.nextInt();
+            if(dicision == 1) {
+                //inout object plus user input fields per movie
+                System.out.print("Enter the title of the film: ");
+                String title = input.next();
+                System.out.print("Enter the director of the film: ");
+                String director = input.next();
+                System.out.print("Enter the year of the film: ");
+                int yearCreated = input.nextInt();
+                System.out.print("Enter if the film is in color(true or false): ");
+                boolean isInColor = input.nextBoolean();
+                System.out.print("Enter the length of the film(in minuts): ");
+                int lengthInMinuts = input.nextInt();
+                System.out.print("Enter the genre of the film: ");
+                String genre = input.next();
+                Movie movieOne = new Movie(title, director, yearCreated,
+                        isInColor, lengthInMinuts, genre);
+                movieList.addMovie(movieOne);
+                Movie[] listFirstMovie = movieList.getMovieCollection();
+                System.out.println(listFirstMovie[i].getTitle());
+                i++;
+            } else if (dicision == 2){
+                System.out.println("Quitting...");
+                break;
+            } else {
+                System.out.println("Invalid input");
+            }
+        }
+
 
         //testing the MovieCollection class for the addMovie method
-        MovieCollection movieList = new MovieCollection();
+//        MovieCollection movieList = new MovieCollection();
         //adding the movieOne to the list
-        movieList.addMovie(movieOne);
+//        movieList.addMovie(movieOne);
         //receive the movieList getter and storing it in a Movie class array
-        Movie[] listFirstMovie = movieList.getMovieCollection();
+//        Movie[] listFirstMovie = movieList.getMovieCollection();
         //output the array with the first movie attributes with index 0 (movieOne)
-        System.out.println(listFirstMovie[0].getTitle());
+//        System.out.println(listFirstMovie[0].getTitle());
 
     }
 
